88bbbed1071e29fb3a8a57dc98ac6c24b07abfc1
Schuyler Duveen <sky@skyb.us>
2010-05-05 11:08:41 -0400

    added documentation to top of __init__ file

--- xmeml/__init__.py
+++ xmeml/__init__.py
@@ -2,9 +2,27 @@ import xml.dom.minidom as dom
 from uuid import uuid1
 from copy import deepcopy
 
+"""
+from xmeml import VideoSequence
+v1 = VideoSequence(file='examples/Conecta Test XML.xml')
+v2 = VideoSequence(file='examples/Mark P XML.xml')
+#print len(v1.track_items)
+
+clip1 = v1.clip(1000, 4000, units='frames')
+clip2 = v2.clip(300, 500, units='seconds')
+
+xmldom1,dumb_uuid = v1.clips2dom([clip1])
+xmldom2,dumb_uuid = v2.clips2dom([clip2])
+
+#print xmldom1.toprettyxml() #probably won't work in Final Cut Pro
+print xmldom2.toxml()
+
+"""
+
 #### TODO
 # ...
 # * splice transitionitems, too
+####
 
 ###XML methods for stupid-XML structures like <clipitem> and <file>
 def xmltextkey(domnode, key):
